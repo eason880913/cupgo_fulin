198d1f96502567adcb0c1126b17708b552049992
李一宸 <>
2020-05-08 17:26:19 +0800

    carousel test

--- main.py
+++ main.py
@@ -176,21 +176,32 @@ def Carousel_Template():
                     ]
                 ),
                 CarouselColumn(
-                    # thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png',
-                    title='這是第三個模塊',
-                    text='最多可以放十個',
+                    # thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
+                    title='這是第二塊模板',
+                    text='副標題可以自己改',
                     actions=[
                         PostbackTemplateAction(
                             label='回傳一個訊息',
-                            data='這是ID=3'
+                            data='這是ID=2'
                         ),
                         MessageTemplateAction(
                             label='用戶發送訊息',
-                            text='我知道這是3'
+                            text='我知道這是2'
                         ),
                         URITemplateAction(
-                            label='uri2',
-                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png'
+                            label='進入2的網頁',
+                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Number_2_in_light_blue_rounded_square.svg/200px-Number_2_in_light_blue_rounded_square.svg.png'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    # thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png',
+                    title='清空購物車',
+                    text='是否確定清空購物車',
+                    actions=[
+                        MessageTemplateAction(
+                            label='是',
+                            text='清空我的購物車' 
                         )
                     ]
                 )
