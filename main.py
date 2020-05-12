d23f0ea8f81cb66928b8c103703c65dd1f2c45ce
李一宸 <>
2020-05-12 21:17:33 +0800

    carousel done

--- main.py
+++ main.py
@@ -139,45 +139,45 @@ def Carousel_Template():
             columns=[
                 CarouselColumn(
                     # thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Number_1_in_green_rounded_square.svg/200px-Number_1_in_green_rounded_square.svg.png',
-                    title='這是第一塊模板',
-                    text='一個模板可以有三個按鈕',
+                    title='訂購鬆餅',
+                    text='請選擇您要的商品',
                     actions=[
-                        PostbackTemplateAction(
-                            label='回傳一個訊息',
-                            data='將這個訊息偷偷回傳給機器人'
+                        MessageTemplateAction(
+                            label='原味鬆餅 $35',
+                            text='訂購原味鬆餅'
                         ),
                         MessageTemplateAction(
-                            label='用戶發送訊息',
-                            text='我知道這是1'
+                            label='巧克力鬆餅 $40',
+                            text='訂購巧克力鬆餅'
                         ),
-                        URITemplateAction(
-                            label='進入1的網頁',
-                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Number_1_in_green_rounded_square.svg/200px-Number_1_in_green_rounded_square.svg.png'
+                        MessageTemplateAction(
+                            label='蜂蜜鬆餅 $40',
+                            text='訂購蜂蜜鬆餅'
                         )
                     ]
                 ),
                 CarouselColumn(
                     # thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
-                    title='這是第二塊模板',
-                    text='副標題可以自己改',
+                    title='訂購拿鐵咖啡',
+                    text='請選擇您要的商品',
                     actions=[
-                        PostbackTemplateAction(
-                            label='回傳一個訊息',
-                            data='這是ID=2'
+                        MessageTemplateAction(
+                            label='Hot/S $35',
+                            text='訂購小杯熱拿鐵'
                         ),
                         MessageTemplateAction(
-                            label='用戶發送訊息',
-                            text='我知道這是2'
+                            label='Hot/M $45',
+                            text='訂購中杯熱拿鐵'
                         ),
-                        URITemplateAction(
-                            label='進入2的網頁',
-                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Number_2_in_light_blue_rounded_square.svg/200px-Number_2_in_light_blue_rounded_square.svg.png'
+                        MessageTemplateAction(
+                            label='Ice/M $45',
+                            text='訂購中杯冰拿鐵'
                         )
                     ]
                 ),
                 CarouselColumn(
                     # thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
-                    title='美式咖啡',
+                    title='訂購美式咖啡',
                     text='請選擇您要的商品',
                     actions=[
                         MessageTemplateAction(
